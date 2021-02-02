815864e948a8b73194fc9c2ca01f9cd44ab826f0
Bjoern <berlach@ccrma.stanford.edu>
2021-02-02 19:19:56 +0100

    First attempt at conditioning

--- mimikit/data/__init__.py
+++ mimikit/data/__init__.py
@@ -4,3 +4,4 @@ from .factory import make_root_db, AudioFileWalker
 from .metadata import Metadata
 from .transforms import file_to_fft, default_extract_func, N_FFT, HOP_LENGTH, SR
 from .freqnet_db import freqnet_db
+from .data_obj import DataObj, SequenceSlicer
